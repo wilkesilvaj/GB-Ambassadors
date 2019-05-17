be2d4124e8465a4834e49aa64aaa202496032b7e
Joao Vitor Wilke Silva <wilkesilvaj@student.douglascollege.ca>
2019-05-17 15:16:56 -0700

    Started developing the possible results divs to form

--- js/calculator.js
+++ js/calculator.js
@@ -21,30 +21,56 @@ var selectID = "championships";
 
 function getElements()  {
     calculatorForm = document.getElementById("calculatorForm");     
-    alert(calculatorForm);
+  
     addChampionship();
 
 }
 
 function addChampionship()  {
     
+    // Creates the new <div> for the each new ROW of the form
+    var formRow = document.createElement("div");
+    formRow.className = "form-row";
+
+    // Creates the new <div> for the each new COLUMN of the form
+    var formCol = document.createElement("div");
+    formCol.className = "form-group col-";
+
+    // Adds new ROW DIV to form
+    calculatorForm.appendChild(formRow);
+
+
+
+    // Creates the new <SELECT> for Championships and assigns an ID to it
     var championshipsComboBox = document.createElement("select");
     championshipsComboBox.id = selectID + countChampionships;
     
-    countChampionships++; // Increment unique ID identifier
-    calculatorForm.appendChild(championshipsComboBox);
-    
+    // Increment unique ID identifier of the <SELECT>
+    countChampionships++; 
 
+    // Adds FIRST COLUMN to ROW
+    formRow.appendChild();
+
+    // Adds select to the page
+    formRow.appendChild(championshipsComboBox);
+    
+    // Populates the new <SELECT> with the championships from the array
     for (var i = 0; i < championshipList.length; i++)   {
+        // Creates new options
         var option = document.createElement("option");
         option.value = championshipList[i];
         option.text = championshipList[i];
         championshipsComboBox.appendChild(option);
     }    
 
+    // Creates the new icon to add new championships
     var icon = document.createElement("i");
     icon.className = "fas fa-plus-square";
-    calculatorForm.appendChild(icon);
+    
+    // Adds the icon to the form
+    formRow.appendChild(icon);
+
+    // Adds event handler to create more championships by clicking the icon
     icon.addEventListener("click", addChampionship, false);
 
     
