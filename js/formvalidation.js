8712eecf35c5635c32a6ea6aeb1e3d8a2197a114
avpa11 <alinavpak@gmail.com>
2019-05-21 23:10:44 -0700

    More or less complete validation of the Adults' form

--- js/formvalidation.js
+++ js/formvalidation.js
@@ -1,17 +1,7 @@
 function validateForm() {
-    var namePattern = /^[A-Z][a-z]{1,20}$/;
     /**
      * REMARK
-     *  
-     * I wasn't able to check on the date
-     * The system uses calendar based on the locale of the device. My Google Chrome Browser is in Russian and the the input is automatically masked as DD.MM.YYYY.
-     * Opera is in English, so the mask there is MM/DD/YYYY
-     * 
-     * I honestly have no idea how to check based on it :(
-     * 
-     * So for now I only added validation for the name, email and phone...
-     * 
-     * One more thing
+     *
      * I used one alert to show the error message and it's triggered after clicking the button. Another approach of how to do it is to use Popovers. In that case 
      * we can use different popovers for different inputs and dynamically check for pattern. I think that the second idea will be a little bit better, but I wasn't
      * able to implement it...
@@ -25,10 +15,14 @@ function validateForm() {
     var birhDatePattern2 = /^(((0)[0-9])|((1)[0-2]))(\/)([0-2][0-9]|(3)[0-1])(\/)(19[0-9][0-9]|200[0-2])$/;
     // THIS ONE CHECKS FOR yyyy-mm-dd
     var birhDatePattern3 = /^(19[0-9][0-9]|200[0-2])(-)(((0)[0-9])|((1)[0-2]))(-)([0-2][0-9]|(3)[0-1])$/;
-    // OERALL BIRTH PATTERN
+    // OVERALL BIRTH PATTERN
     var birhDatePattern = /^((19[0-9][0-9]|200[0-2])(-)(((0)[0-9])|((1)[0-2]))(-)([0-2][0-9]|(3)[0-1]))|((((0)[0-9])|((1)[0-2]))(\/)([0-2][0-9]|(3)[0-1])(\/)(19[0-9][0-9]|200[0-2]))|(\s*(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.(19[0-9][0-9]|200[0-2])\s)$/;
+    var namePattern = /^[A-z ]{1,20}$/;
+    var gradPattern = /^((19[6-9][0-9]|20[0-9][0-9])(-)(((0)[0-9])|((1)[0-2]))(-)([0-2][0-9]|(3)[0-1]))|((((0)[0-9])|((1)[0-2]))(\/)([0-2][0-9]|(3)[0-1])(\/)(19[0-9][0-9]|200[0-2]))|(\s*(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.(19[0-9][0-9]|200[0-2])\s)$/;
     var phonePattern = /^(1 )*\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})|(\d){3}(-)*(\d){4}$/;
-    var emailPattern = /^[_a-zA-Z\\-]+(\.[_a-zA-Z0-9\\-]+)*@[a-zA-Z0-9\\-]+(\.[a-zA-Z0-9\\-]+)*(\.[a-z]{2,6})$/;
+    var emailPattern = /^[_a-zA-Z0-9\\-]+(\.[_a-zA-Z0-9\\-]+)*@[a-zA-Z0-9\\-]+(\.[a-zA-Z0-9\\-]+)*(\.[a-z]{2,6})$/;
+    var addressPattern = /^[0-9A-z, -]{1,50}$/;
+    var schoolPattern = /^[A-z ]{3,30}$/;
     
     var firstName = document.getElementById("firstName").value;
 
@@ -36,9 +30,17 @@ function validateForm() {
 
     var DOB = document.getElementById("dateOfBirth").value;
 
+    var graduation = document.getElementById("dateOfGraduation").value;
+
     var phone = document.getElementById("phoneNumber").value;
 
     var email = document.getElementById("email").value;
+
+    var address = document.getElementById("address").value;
+
+    var school = document.getElementById("homeSchool").value;
+
+    var schoolAdress = document.getElementById("homeSchoolAddress").value;
 
     var alert = document.getElementById("alert");
 
@@ -49,16 +51,6 @@ function validateForm() {
         errorMessage.innerHTML = "Please enter a valid first name";
         closeAlert();
     }
-    
-    //to check for date pf birth
-    if (birhDatePattern.test(DOB) == false) {
-        window.scrollTo(0, 0);
-        alert.style.display = "block";
-        errorMessage.innerHTML = "Please enter a valid date of birth";
-        closeAlert();
-    }
-    
-
     //to check for last name
     else if (namePattern.test(lastName) == false) {
         window.scrollTo(0, 0);
@@ -66,7 +58,20 @@ function validateForm() {
         errorMessage.innerHTML = "Please enter a valid last name";
         closeAlert();
     }
-
+    //to check for date of birth
+    else if (birhDatePattern.test(DOB) == false) {
+        window.scrollTo(0, 0);
+        alert.style.display = "block";
+        errorMessage.innerHTML = "Please enter a valid date of birth";
+        closeAlert();
+    }
+    // to check for the graduation (belt) 
+    else if (gradPattern.test(graduation) == false) {
+    window.scrollTo(0, 0);
+    alert.style.display = "block";
+    errorMessage.innerHTML = "Please enter a valid graduation date";
+    closeAlert();
+    }
     //to check for phone number
     else if (phonePattern.test(phone) == false) {
         window.scrollTo(0, 0);
@@ -81,6 +86,34 @@ function validateForm() {
         errorMessage.innerHTML = "Please enter a vaild email address";
         closeAlert();
     }
+    //to check for the address
+    else if (addressPattern.test(address) == false) {
+        window.scrollTo(0, 0);
+        alert.style.display = "block";
+        errorMessage.innerHTML = "Please enter a vaild address";
+        closeAlert();
+    }
+    //to check for school
+    else if (schoolPattern.test(school) == false) {
+        window.scrollTo(0, 0);
+        alert.style.display = "block";
+        errorMessage.innerHTML = "Please enter a vaild name of your home school";
+        closeAlert();
+    }
+    //to check for school address
+    else if (addressPattern.test(schoolAdress) == false) {
+        window.scrollTo(0, 0);
+        alert.style.display = "block";
+        errorMessage.innerHTML = "Please enter a vaild school address";
+        closeAlert();
+    }
+    // if any checkbox is not checked
+    else if (isChecked() == false) {
+        window.scrollTo(0, 0);
+        alert.style.display = "block";
+        errorMessage.innerHTML = "Please make sure to answer all questions";
+        closeAlert();
+    }
     // if everything is correct
     else if (namePattern.test(firstName) == true && namePattern.test(lastName) == true && phonePattern.test(phone) == true && emailPattern.test(email) == true) {
         window.scrollTo(0, 0);
@@ -88,6 +121,20 @@ function validateForm() {
         errorMessage.innerHTML = "Thank you for submitting the form";
         closeAlert();
     }
+}
+
+// function to check if all the radiobuttons are checked
+function isChecked() {
+    if ($('input[name=rbdGender]:checked').length > 0 &&
+    $('input[name=rdbBelt]:checked').length > 0 &&
+    $('input[name=icp6]:checked').length > 0 &&
+    $('input[name=icpUpToDate]:checked').length > 0 &&
+    $('input[name=uniform]:checked').length > 0 &&
+    $('input[name=attendance]:checked').length > 0 &&
+    $('input[name=results]:checked').length > 0) {
+        return true;
+    }
+    return false;
 }
 
 function closeAlert() {
